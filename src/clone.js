062e5f0f0a08f49399e1f6cc5c8f56df44f021e9
wenyejie <wenyejie@swipal.com>
2019-11-19 10:14:21 +0800

    重新调整克隆方法

--- src/clone.js
+++ src/clone.js
@@ -4,25 +4,29 @@ import { hasOwn, isArray, isObject } from './index.js'
  * 对对象进行拷贝
  * @param obj {*} 拷贝对象
  * @param deep? {Boolean} 是否深拷贝, 默认为true
+ * @param weakMap? {WeakMap} 引用对象
  * @return {*}
  */
-export const clone = (obj, deep = true) => {
+export const clone = (obj, deep = true, weakMap = new WeakMap()) => {
   if (!isObject(obj)) {
     return obj
   }
 
-  if (isArray(obj)) {
-    return obj.map(item => (deep ? clone(item, deep) : item))
+  if (weakMap.get(obj)) {
+    return weakMap.get(obj)
   }
 
-  const newObj = {}
-  for (let key in obj) {
-    if (!hasOwn(obj, key)) {
-      continue
-    }
-    newObj[key] = deep ? clone(obj[key], deep) : obj[key]
+  const result = isArray(obj) ? [] : {}
+  weakMap.set(obj, result)
+  const keys = Object.keys(obj)
+  let key
+  let temp
+  for (let i = 0, length = keys.length; i < length; i++) {
+    key = keys[i]
+    temp = obj[key]
+    result[key] = clone(temp, deep, weakMap)
   }
-  return newObj
+  return result
 }
 
 export default clone
