ef10d0d172332942c4d92fd68fd7306db933e6bf
wenyejie <wenyejie@swipal.com>
2019-11-26 10:35:13 +0800

    优化调整storage方法

--- src/storage.js
+++ src/storage.js
@@ -1,44 +1,50 @@
 import { emptyObject } from './common.js'
 import { inBrowser } from './env.js'
 
-const storage = name => {
-  const s = inBrowser ? window[`${name}Storage`] : emptyObject
-  return {
-    get(key) {
-      let result
-      try {
-        result = (JSON.parse(s.getItem(key)) || emptyObject).v
-      } catch (e) {
-        console.error(e)
-      }
-      return result
-    },
+class CustomStorage {
+  #storage = emptyObject
+  constructor(name) {
+    this.#storage = inBrowser ? window[`${name}Storage`] : emptyObject
+  }
 
-    set(key, value) {
-      try {
-        s.setItem(key, JSON.stringify({ v: value }))
-      } catch (e) {
-        console.error(e)
-      }
-    },
-
-    remove(key) {
-      s.removeItem(key)
-    },
-
-    clear() {
-      s.clear()
-    },
-
-    length() {
-      return s.length
+  get(key) {
+    let result
+    try {
+      result = (JSON.parse(this.#storage.getItem(key)) || emptyObject).v
+    } catch (e) {
+      console.error(e)
     }
+    return result
+  }
+
+  set(key, value) {
+    try {
+      this.#storage.setItem(key, JSON.stringify({ v: value }))
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  remove(key) {
+    this.#storage.removeItem(key)
+  }
+
+  clear() {
+    this.#storage.clear()
+  }
+
+  key(index) {
+    return this.#storage.key(index)
+  }
+
+  length() {
+    return this.#storage.length
   }
 }
 
-export const local = storage('local')
+export const local = new CustomStorage('local')
 
-export const session = storage('session')
+export const session = new CustomStorage('session')
 
 export default {
   local,
