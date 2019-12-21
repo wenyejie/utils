b3324bdae117d6d7ebee91d3d38ebaf2aa53e3f7
wenyejie <wenyejie@swipal.com>
2019-12-21 09:30:31 +0800

    新增相关正则表达式

--- src/regexp.js
+++ src/regexp.js
@@ -8,13 +8,22 @@ export const telephone = /^(\d{3,4}-)?\d{7,8}$/
 export const phone = /(^1\d{10}$)|(^(0\d{2,3}-?)?\d{7,8}(-\d{3,})?$)/
 
 // 身份证号码
-export const IDCardNumber = /^[1-9][0-9]{5}(19|20)[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|31)|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}([0-9]|X)$/
+export const IDCardNumber = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/
 
 // 银行卡号
 export const bankCardNumber = /^\d{16,19}$/
 
 // 中文名称
 export const chineseName = /^[\u4E00-\u9FA5|·]{2,}$/
+
+// 邮箱地址
+export const email = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+
+// url地址
+export const url = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i
+
+// 邮编
+export const postcode = /^[1-9]\d{5}(?!\d)$/
 
 export default {
   phone,
