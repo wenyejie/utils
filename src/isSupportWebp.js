a431c13c9c8e08abd73360dae2b17919169faf31
wenyejie <wenyejie@swipal.com>
2020-01-07 11:34:18 +0800

    新增判断浏览器是否支持webp

---
+++ src/isSupportWebp.js
@@ -0,0 +1,20 @@
+export const isSupportWebp = () => {
+  const webpBase64 = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA='
+  const image = new Image()
+  return new Promise((resolve, reject) => {
+    image.onload = () => {
+      const result = image.width > 0 && image.height > 0
+      if (result) {
+        resolve()
+      } else {
+        reject()
+      }
+    }
+    image.onerror = () => {
+      reject()
+    }
+    image.src = webpBase64
+  })
+}
+
+export default isSupportWebp
