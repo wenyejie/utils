86880b92720ebf3ff33db6cf2c73cd00df964687
wenyejie <wenyejie@swipal.com>
2020-03-09 14:41:42 +0800

    增加arrayRemove方法

--- src/array.js
+++ src/array.js
@@ -3,6 +3,17 @@ import { isArray, isObject } from './common.js'
 
 export const arrayUnique = array => Array.from(new Set(array))
 
+// 移除数组子项
+export const arrayRemove = (array, item) => {
+  if (Array.isArray(array) && array.length) {
+    const index = array.indexOf(item)
+    if (index > -1) {
+      array.splice(index, 1)
+    }
+  }
+  return array
+}
+
 /**
  * 根据属性从数组中获取对象
  * @param array {Object[]}
