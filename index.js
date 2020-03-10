aef5014360ad7967571999b8044957d49bad78ae
wenyejie <wenyejie@swipal.com>
2020-03-10 16:16:40 +0800

    变更index.js地址

---
+++ index.js
@@ -0,0 +1,103 @@
+export {
+  noop,
+  emptyObject,
+  nullObject,
+  hasOwnProperty,
+  hasOwn,
+  isEmptyValue,
+  isBigint,
+  isBoolean,
+  each,
+  objectToString,
+  toTypeString,
+  toRawType,
+  isFunction,
+  isSymbol,
+  isPrimitive,
+  isPromise,
+  once,
+  cached,
+  swap,
+  zeroize
+} from './src/common.js'
+
+export {
+  isNumber,
+  isPlainNumber,
+  isInteger,
+  isNaturalNumber,
+  isPositiveInteger,
+  exactCalc,
+  randomInteger,
+  randomNumber,
+  isEvenNumber,
+  isOddNumber
+} from './src/number.js'
+
+export {
+  isDate,
+  isPlainDate,
+  isOneDay,
+  dateConvert,
+  dateFormat,
+  monthLength,
+  isOneMonth,
+  isOneYear
+} from './src/date.js'
+
+export { isString, getSuffix } from './src/string.js'
+
+export { throttle } from './src/throttle.js'
+
+export { debounce } from './src/debounce.js'
+
+export { clone } from './src/clone.js'
+
+export { local, session } from './src/storage.js'
+
+export { isElement, isComment, isText } from './src/dom.js'
+
+export {
+  inBrowser,
+  isAndroid,
+  isChrome,
+  isEdge,
+  isFirefox,
+  isIE,
+  isIE9,
+  isIos,
+  isIPad,
+  isIPhone,
+  isMobile,
+  isPc,
+  isPhantom,
+  isWeChat,
+  ua
+} from './src/env.js'
+
+export { bankCardFormat, moneyFormat, number2cn, queryInfoByCardNumber } from './src/format.js'
+
+export { isSupportWebp } from './src/isSupportWebp.js'
+
+export { arrayUnique, findByProps, isArray, arrayRemove } from './src/array.js'
+
+export { pubSub } from './src/pubSub.js'
+
+export {
+  isRegExp,
+  IDCardNumber,
+  email,
+  telephone,
+  phone,
+  bankCardNumber,
+  chineseName,
+  mobilePhone,
+  postcode,
+  url
+} from './src/regexp.js'
+
+export { objectBan, objectPick, objectLength, isObject, isPlainObject } from './src/object.js'
+
+export { url2https } from './src/url2https.js'
+
+export { checkIDCard } from './src/validate.js'
