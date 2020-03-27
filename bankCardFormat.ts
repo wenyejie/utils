e3c95a5d0b437efb5fca6389a541bdefcf40c39a
wenyejie <wenyejie@swipal.com>
2020-03-27 15:36:21 +0800

    增加正则校验

--- bankCardFormat.ts
+++ bankCardFormat.ts
@@ -1,11 +1,12 @@
 import isString from './isString'
+import { rBankCardNO } from './regexp'
 
 /**
  * 银行卡格式化
  * @param card 卡号
  */
 export const bankCardFormat = (card: string) => {
-  if (!isString(card)) {
+  if (!isString(card) || rBankCardNO.test(card)) {
     return ''
   }
   card = card.trim()
