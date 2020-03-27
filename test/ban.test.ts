4ff3e236b20ecb4f3698402202385e88be7a0ef8
wenyejie <wenyejie@swipal.com>
2020-03-27 15:36:43 +0800

    增加测试用例覆盖

--- test/ban.test.ts
+++ test/ban.test.ts
@@ -1,9 +1,42 @@
 import ban from '../ban'
 
-test('ban', () => {
-  const obj = {
+const obj = {
+  name: 'test',
+  age: 28,
+  message: ''
+}
+
+test('ban---移除name', () => {
+  expect(ban(obj, 'name')).toStrictEqual({
+    age: 28,
+    message: ''
+  })
+})
+
+test('ban---移除没有的属性', () => {
+  expect(ban(obj, 'name1')).toStrictEqual({
     name: 'test',
-    age: 28
-  }
-  expect(ban(obj, 'name')).toStrictEqual({ age: 28 })
+    age: 28,
+    message: ''
+  })
+})
+
+test('ban---移除所有属性', () => {
+  expect(ban(obj, 'name', 'age', 'message')).toStrictEqual({})
+})
+
+test('ban---不移除属性', () => {
+  expect(ban(obj)).toStrictEqual({
+    name: 'test',
+    age: 28,
+    message: ''
+  })
+})
+
+test('ban---非法参数', () => {
+  expect(ban(obj, () => {})).toStrictEqual({
+    name: 'test',
+    age: 28,
+    message: ''
+  })
 })
