b9694ff7066fd48e3590a2ebad9eaf7c8e7676b2
wenyejie <wenyejie@swipal.com>
2020-03-30 17:22:08 +0800

    调整版本号

--- prefix.ts
+++ prefix.ts
@@ -4,11 +4,11 @@ import { isString } from './isString'
  * 获取字符串中的前缀
  * @param string 字符串
  */
-export const prefix = (string?: string): string => {
+export const prefix = (string: string): string => {
   if (!isString(string)) {
     return ''
   }
-  string = <string>string.trim()
+  string = string.trim()
 
   const index = string.search(/^(\w+)+(?=\.)/)
   if (index === -1) {
