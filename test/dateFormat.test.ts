14e82feef35cbaa96c562d85449b0b2a9bc7cc83
wenyejie <wenyejie@swipal.com>
2020-04-01 15:26:17 +0800

    新增dateFormat测试用例

---
+++ test/dateFormat.test.ts
@@ -0,0 +1,11 @@
+import dateFormat from '../dateFormat'
+
+test('dateFormat', () => {
+  const dateString = '2002-02-02 02:02:02'
+  const date = new Date(dateString)
+  const timestamp = date.getTime()
+  expect(dateFormat(date)).toBe(dateString)
+  expect(dateFormat(timestamp)).toBe(dateString)
+  expect(dateFormat(null)).toBe('')
+  expect(dateFormat(dateString)).toBe(dateString)
+})
