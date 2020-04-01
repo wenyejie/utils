77193ddfd73504f10372c3270b9821809210c691
wenyejie <wenyejie@swipal.com>
2020-04-01 18:12:49 +0800

    新增函数防抖方法

---
+++ test/debounce.test.ts
@@ -0,0 +1,14 @@
+import debounce from '../debounce'
+
+// 暂时不知道如何编写测试用例
+// test('debounce', () => {
+//   let run = 0
+//   const demo = debounce(() => {
+//     run++
+//   })
+//
+//   for (let i = 0; i < 5; i++) {
+//     demo()
+//   }
+//   expect(run).toBe(5)
+// })
