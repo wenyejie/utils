742d03e87047cc278077bc97829b72dd85e41d0a
wenyejie <wenyejie@swipal.com>
2020-04-03 15:50:37 +0800

    调整注释和下载失败的处理逻辑

--- downloadFile.ts
+++ downloadFile.ts
@@ -2,6 +2,7 @@ import isString from './isString'
 
 /**
  * 通过url下载
+ * PS: 只能下载同源的链接, 如果不是同源的文件请使用downloadBlob方法
  * @param url
  * @param filename
  */
@@ -13,9 +14,8 @@ export const downloadFile = (url: string, filename: string) => {
   $download.style.display = 'none'
   $download.href = url
   $download.setAttribute('download', filename)
-  if ($download.download === undefined) {
-    $download.setAttribute('target', '_blank')
-  }
+  // 当不允许下载或者下载失败时, 在新页面打开, 而不是当前页面
+  $download.setAttribute('target', '_blank')
   document.body.appendChild($download)
   $download.click()
   document.body.removeChild($download)
