654ccfa8fc21d5745d85c5e7ed858035b4f20ed9
wenyejie <wenyejie@swipal.com>
2020-04-03 16:07:02 +0800

    优化没有获取到文件名后缀时, 自动从type中获取

--- downloadBlob.ts
+++ downloadBlob.ts
@@ -1,4 +1,6 @@
 import downloadFile from './downloadFile'
+import isBlob from './isBlob'
+import suffix from './suffix'
 
 /**
  * 下载blob文件
@@ -6,6 +8,13 @@ import downloadFile from './downloadFile'
  * @param filename
  */
 export const downloadBlob = (blob: Blob, filename: string) => {
+  if (!isBlob(blob)) {
+    throw new Error('参数错误: blob不是Blob类型')
+  }
+  if (!suffix(filename)) {
+    filename = filename + '.' + blob.type.replace(/image\//, '')
+    console.warn('参数错误: filename没有后缀名')
+  }
   if (window.navigator.msSaveBlob) {
     window.navigator.msSaveBlob(blob, filename)
     return
