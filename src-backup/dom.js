316632f6153a9628f79f6d373733128550471c02
wenyejie <wenyejie@swipal.com>
2020-04-03 10:38:19 +0800

    移除冗余备份

--- src-backup/dom.js
+++
@@ -1,5 +0,0 @@
-export const isElement = obj => (obj ? obj.nodeType === 1 : false)
-
-export const isComment = obj => (obj ? obj.nodeType === 8 : false)
-
-export const isText = obj => (obj ? obj.nodeType === 3 : false)
--- src-backup/url2https.js
+++
@@ -1,8 +0,0 @@
-export const url2https = url => {
-  if (/^https/.test(url)) {
-    return url
-  }
-  return url.replace(/^http/, 'https')
-}
-
-export default url2https
