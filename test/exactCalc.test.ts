e64bf40fd319216e88880536ef4514567fb08121
wenyejie <wenyejie@swipal.com>
2020-04-18 15:10:41 +0800

    移除exactCalc方法, 发现严重bug

--- test/exactCalc.test.ts
+++
@@ -1,8 +0,0 @@
-import exactCalc from '../exactCalc'
-
-test('exactCalc', () => {
-  expect(exactCalc(0.1, 0.2, '+')).toBe(0.3)
-  expect(exactCalc(0.1, 0.2, '*')).toBe(0.02)
-  expect(exactCalc(11.1, 0.2, '/')).toBe(55.5)
-  expect(exactCalc(0.3, 0.2, '-')).toBe(0.1)
-})
