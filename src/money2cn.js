e460e83c5895253d2f43e5bce8975666798f64ff
wenyejie <wenyejie@swipal.com>
2020-07-07 10:30:00 +0800

    重写金额转中文大写

--- src/money2cn.js
+++ src/money2cn.js
@@ -2,32 +2,67 @@
  * 金额转中文大写
  * @param n
  */
-export const money2cn = n => {
-  const fraction = ['角', '分']
-  const digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
-  const unit = [
-    ['元', '万', '亿'],
-    ['', '拾', '佰', '仟']
-  ]
-  n = Math.abs(n)
-  let s = ''
-  for (let i = 0; i < fraction.length; i++) {
-    s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '')
-  }
-  s = s || '整'
-  n = Math.floor(n)
-  for (let i = 0; i < unit[0].length && n > 0; i++) {
-    let p = ''
-    for (let j = 0; j < unit[1].length && n > 0; j++) {
-      p = digit[n % 10] + unit[1][j] + p
-      n = Math.floor(n / 10)
+const fraction = ['角', '分']
+const digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
+const numberDigit = ['', '拾', '佰', '仟']
+const unit = ['', '万', '亿']
+const rNumberDigit = /零[拾佰仟]/g
+const rUnit = /零+([万亿])/g
+const rZero = /零{2,}/
+const money2cn = n => {
+  let result = ''
+  const integerAry = n
+    .toString()
+    .replace(/\.\d+$/, '')
+    .split('')
+
+  const decimalAry = n
+    .toString()
+    .replace(/^\d+\.?/, '')
+    .split('')
+
+  for (let i = integerAry.length - 1, j = 0; i >= 0; i--, j++) {
+    if (integerAry[i] === '0' && i > 0 && i < integerAry.length - 1) {
+      integerAry[i] = '零'
+    } else {
+      integerAry[i] = digit[integerAry[i]] + numberDigit[j % 4]
     }
-    s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s
+    integerAry[i] += j % 4 === 0 ? unit[j / 4] : ''
   }
-  return s
-    .replace(/(零.)*零元/, '元')
-    .replace(/(零.)+/g, '零')
-    .replace(/^整$/, '零元整')
+
+  for (let i = 0; i < decimalAry.length; i++) {
+    if (decimalAry[i] !== '0') {
+      decimalAry[i] = digit[decimalAry[i]] + fraction[i]
+    } else {
+      decimalAry[i] = ''
+    }
+  }
+
+  let integer = integerAry
+    .join('')
+    .replace(rNumberDigit, '')
+    .replace(rUnit, '$1')
+    .replace(rZero, '零')
+
+  if (integer.length > 1) {
+    integer = integer.replace(/零$/, '')
+  }
+
+  if (integer.length >= 1) {
+    integer += '元'
+  }
+
+  let decimal = decimalAry.join('')
+
+  if (decimal === '') {
+    decimal = '整'
+  } else {
+    integer = integer.replace(/^零元$/, '')
+  }
+
+  result = integer + decimal
+
+  return result
 }
 
 export default money2cn
