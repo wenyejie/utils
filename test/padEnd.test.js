83ed8ad6e08a6c3c087d18d18a015fb15160acb4
yejie.wen <yejie.wen@hoo.com>
2021-04-09 17:14:34 +0800

    调整测试用例的名称

--- test/padEnd.test.js
+++ test/padEnd.test.js
@@ -1,6 +1,6 @@
 import padEnd from '../src/padEnd.js'
 
-test('phoneNumFormat', () => {
+test('padEnd', () => {
   expect(padEnd(4)).toBe('40')
   expect(padEnd(NaN)).toBe('00')
 })
--- test/padStart.test.js
+++ test/padStart.test.js
@@ -1,6 +1,6 @@
 import padStart from '../src/padStart.js'
 
-test('phoneNumFormat', () => {
+test('padStart', () => {
   expect(padStart(4)).toBe('04')
   expect(padStart(NaN)).toBe('00')
 })
