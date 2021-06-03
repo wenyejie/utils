f2bebcef3598e87cced29c9928bf0c0c37c9090e
yejie.wen <yejie.wen@hoo.com>
2021-06-03 10:48:01 +0800

    调整测试用例

--- test/isSameDay.test.js
+++ test/isSameDay.test.js
@@ -3,6 +3,6 @@ import isSameDay from '../src/isSameDay'
 test('isSameDay', () => {
   const date = new Date('2020-02-02')
   expect(isSameDay(new Date(), new Date())).toBeTruthy()
-  expect(isSameDay(date)).toBeFalsy()
+  expect(isSameDay(date, new Date())).toBeFalsy()
   expect(isSameDay(date, 1580601600000)).toBeTruthy()
 })
