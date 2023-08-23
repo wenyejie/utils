97467ed2f3cc9691c8478efc76aa653d24a75e71
wenyejie <wenyejie@foxmail.com>
2023-08-23 18:29:40 +0800

    新增测试用例

---
+++ tests/isPrimitive.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from 'vitest'
+import isPrimitive from '../src/isPrimitive'
+
+test('isPrimitive', () => {
+  expect(isPrimitive(undefined)).toBeTruthy()
+  expect(isPrimitive(null)).toBeTruthy()
+  expect(isPrimitive('string')).toBeTruthy()
+  expect(isPrimitive(true)).toBeTruthy()
+  expect(isPrimitive(Symbol('symbol'))).toBeTruthy()
+  expect(isPrimitive(1)).toBeTruthy()
+  expect(isPrimitive(1n)).toBeTruthy()
+
+  expect(isPrimitive({})).toBeFalsy()
+  expect(isPrimitive(() => {})).toBeFalsy()
+  expect(isPrimitive([])).toBeFalsy()
+  expect(isPrimitive(new Date())).toBeFalsy()
+  expect(isPrimitive(new Map())).toBeFalsy()
+  expect(isPrimitive(new Set())).toBeFalsy()
+  expect(isPrimitive(new WeakSet())).toBeFalsy()
+  expect(isPrimitive(new WeakMap())).toBeFalsy()
+
+})
