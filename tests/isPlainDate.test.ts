1b25db9377e3c77c6bb7898374989fea05467242
wenyejie <wenyejie@foxmail.com>
2023-08-28 15:43:04 +0800

    新增isPlainDate测试用例

---
+++ tests/isPlainDate.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from 'vitest'
+import isPlainDate from '../src/isPlainDate'
+
+test('isPlainDate', () => {
+  expect(isPlainDate(new Date())).toBeTruthy()
+  expect(isPlainDate(new Date('dddd'))).toBeTruthy()
+
+  expect(isPlainDate(11)).toBeFalsy()
+})
