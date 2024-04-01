2572436bad33d7b9d0b6d80f77fb04b295f3bdb4
wenyejie <youpingzou7@gmail.com>
2024-04-01 17:00:14 +0800

    变更版本号

--- src/wenyejie.ts
+++ src/wenyejie.ts
@@ -1,5 +1,5 @@
 // @Copyright by https://github.com/wenyejie/utils
-export const VERSION = '2.2.0'
+export const VERSION = '2.2.1'
 export * from './arrDelItemByProp'
 export * from './arrDelItemByVal'
 export * from './arrDownItem'
