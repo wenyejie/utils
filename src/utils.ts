2efdbb887228b609d109fffe5a1935eb7724a3e1
Storm <wenyejie@foxmail.com>
2024-08-08 17:57:55 +0800

    添加注释

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,21 @@
+// 一年的毫秒
 export const TS_YEAR = 31536e6
+// 一周的毫秒
 export const TS_WEEK = 6048e5
+// 一天的毫秒
 export const TS_DAY = 864e5
+// 一小时的毫秒
 export const TS_HOUR = 36e5
+// 一分钟的毫秒
 export const TS_MINUTE = 6e4
+// 一秒钟的毫秒
 export const TS_SECOND = 1e3
+// 一毫秒
 export const TS_MILLISECOND = 1
 
-// 时间单位
+/**
+ * 时间单位常量集合
+ */
 export enum TS_UNIT {
   YEAR = TS_YEAR,
   WEEK = TS_WEEK,
