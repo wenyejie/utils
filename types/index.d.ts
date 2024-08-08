c1dd70768abbcd0dc17735c3d04acf86f6c0b04f
Storm <wenyejie@foxmail.com>
2024-08-08 17:12:12 +0800

    变更type声明注释

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,7 +10,7 @@ export type AnyFn = (...rest: unknown[]) => unknown
 // 是对象的其中几个
 export type valueOf<T> = T[keyof T]
 
-// 是对象的其中几个或者其中的几个类型
+// 是对象的其中几个值的类型或者对象中的其中几个属性
 export type PartialValueOf<T> = Partial<T> | valueOf<T>
 
 export type PartialToRawTypeKeyof<T> = Partial<Record<ToRawType, keyof T>>
